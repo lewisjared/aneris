3e378137776816820f146db3923cefadec28a473
Jared Lewis <jared@jared.kiwi.nz>
2022-10-12 20:57:13 +1100

    Onyl load units once

--- aneris/unit_registry.py
+++ aneris/unit_registry.py
@@ -1,7 +1,8 @@
 from scmdata.units import _unit_registry as unit_registry
 
-unit_registry.define("H = [hydrogen]")
-unit_registry.define("hydrogen = H")
-unit_registry.define("t{symbol} = t * {symbol}".format(symbol="H"))
+if not hasattr(unit_registry, "hydrogen"):
+    unit_registry.define("H = [hydrogen] = H2")
+    unit_registry.define("hydrogen = H")
+    unit_registry.define("t{symbol} = t * {symbol}".format(symbol="H"))
 
 ur = unit_registry

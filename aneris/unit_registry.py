abc7c4df3c9bb9a0ef765717973742638216d0a8
Jared Lewis <jared@jared.kiwi.nz>
2022-12-12 09:34:58 +1100

    Use openscm_units unit registry

--- aneris/unit_registry.py
+++ aneris/unit_registry.py
@@ -1,8 +1,4 @@
-from scmdata.units import _unit_registry as unit_registry
+from openscm_units import unit_registry
 
-if not hasattr(unit_registry, "hydrogen"):
-    unit_registry.define("H = [hydrogen] = H2")
-    unit_registry.define("hydrogen = H")
-    unit_registry.define("t{symbol} = t * {symbol}".format(symbol="H"))
 
 ur = unit_registry
